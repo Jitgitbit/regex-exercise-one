6cdd54aff816d2395cfad9f746bd7fb4ecd9364b
Jitgitbit <rowowolf@gmail.com>
2020-04-30 13:53:06 +0200

    telephone done + refactored

--- validation.js
+++ validation.js
@@ -1,3 +1,7 @@
 var reg = /[a-z]/gi
+var reg2 = new RegExp(/a-z/,'gi')           // these are the two ways of writing RegEx ! line 1 and 2 give the same output
 
-var reg2 = new RegExp(/a-z/,'gi')           // these are the two ways of writing RegEx ! line 1 and 3 give the same output
+const patterns = {
+  // telephone: /^[0-9]{11}$/,
+  telephone: /^\d{11}$/,
+}
