0ebf006d04c7629176cce6a43f87cf7b0408106e
Jitgitbit <rowowolf@gmail.com>
2020-04-30 14:30:15 +0200

    actually puting regex to use

--- validation.js
+++ validation.js
@@ -4,4 +4,21 @@ var reg2 = new RegExp(/a-z/,'gi')           // these are the two ways of writing
 const patterns = {
   // telephone: /^[0-9]{11}$/,
   telephone: /^\d{11}$/,
+};
+
+const inputs = document.querySelectorAll('input');
+
+function validate(field, regex){
+  if(regex.test(field.value)){
+    field.className = 'valid'
+  }else{
+    field.className = 'invalid'
+  }
 }
+
+inputs.forEach((input) => {
+  input.addEventListener('keyup', (e) => {
+    console.log('===>> e.target.attributes.name.value', e.target.attributes.name.value)
+    validate(e.target, patterns[e.target.attributes.name.value])
+  })
+});
